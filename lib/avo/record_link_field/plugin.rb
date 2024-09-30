0d5218cfa970dd48e8ac7cc5a7d791ae2629c59f
Paul Bob <paul.ionut.bob@gmail.com>
2024-09-30 12:47:23 +0300

    refacotr: plugin rework

--- lib/avo/record_link_field/plugin.rb
+++ lib/avo/record_link_field/plugin.rb
@@ -1,11 +1,9 @@
-require_relative "fields/record_link_field"
 
 module Avo
   module RecordLinkField
     class Plugin < Avo::Plugin
       class << self
         def boot
-          Avo.plugin_manager.register_field :record_link, Avo::RecordLinkField::Fields::RecordLinkField
         end
 
         def init
